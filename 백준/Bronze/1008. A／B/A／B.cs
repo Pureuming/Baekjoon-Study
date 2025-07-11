20225106929c969734ffda48edae30d0dacc1e3e
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-07-11 15:52:22 +0900

    [Bronze V] Title: A/B, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,10 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        double a = double.Parse(input[0]);
+        double b = double.Parse(input[1]);
+        Console.WriteLine(a / b);
+    }
+}
