d92440244fe14910967401a458011aafc415e76b
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-07-11 16:07:49 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 52 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,9 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int year = int.Parse(input);
+        Console.WriteLine(year - 543);
+    }
+}
