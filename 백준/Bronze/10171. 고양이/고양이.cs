6074ed13e6efc1d643cb4874c1f2f0b8ac93ad6e
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-07-15 18:53:40 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,10 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
