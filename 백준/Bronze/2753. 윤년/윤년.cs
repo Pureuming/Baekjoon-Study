2107989caf9f028a9f3f4f14210bdf98446aaa2f
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-07-15 19:28:40 +0900

    [Bronze V] Title: 윤년, Time: 72 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,16 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+
+        if (input % 4 == 0 && (input % 100 != 0 || input % 400 == 0))
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+}
