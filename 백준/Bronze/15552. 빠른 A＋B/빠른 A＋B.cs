f9ac227f4dc311b33d21851f681d29311361ec6c
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-07-18 21:39:59 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 884 ms, Memory: 36876 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cs
@@ -0,0 +1,20 @@
+using System.Text;
+
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        StringBuilder answer = new StringBuilder();
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            answer.AppendLine((a + b).ToString());
+        }
+
+        Console.WriteLine(answer.ToString());
+    }
+}
