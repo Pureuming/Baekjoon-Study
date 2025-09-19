078884d16d61020c7e96d280b23021f1a6998811
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-09-19 15:23:42 +0900

    [Bronze V] Title: 직사각형, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,9 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        Console.Write(A * B);
+    }
+}
