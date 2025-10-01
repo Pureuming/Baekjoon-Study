d2802a68bcc2e75704f94d07e221fb9d1e811bc7
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-10-01 16:52:42 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 52 ms, Memory: 5564 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cs
@@ -0,0 +1,8 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        ulong input = ulong.Parse(Console.ReadLine());
+        Console.Write($"{(input - 1) * input / 2}\n2");
+    }
+}
