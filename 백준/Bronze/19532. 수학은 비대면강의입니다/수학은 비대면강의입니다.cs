d273188d1fbc1f1478c9980db4c16d9e2adbe361
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-10-08 01:00:57 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 56 ms, Memory: 6036 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cs
@@ -0,0 +1,25 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        int c = int.Parse(input[2]);
+        int d = int.Parse(input[3]);
+        int e = int.Parse(input[4]);
+        int f = int.Parse(input[5]);
+
+        for (int x = -999; x <= 999; x++)
+        {
+            for (int y = -999; y <= 999; y++)
+            {
+                if (a * x + b * y == c && d * x + e * y == f)
+                {
+                    Console.WriteLine($"{x} {y}");
+                    return;
+                }
+            }
+        }
+    }
+}
