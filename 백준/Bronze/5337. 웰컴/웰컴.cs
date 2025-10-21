0c71732295f5e42bc5bc9d5f4844bfd40c1aed50
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-10-21 21:43:11 +0900

    [Bronze V] Title: 웰컴, Time: 44 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,9 @@
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(".  .   .");
+        Console.WriteLine("|  | _ | _. _ ._ _  _");
+        Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
