692d2dbaa89434c08dc939c7473e6f8a2a1efa49
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-11-20 23:46:11 +0900

    [Silver V] Title: 돌 게임, Time: 52 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cs
@@ -0,0 +1,14 @@
+using System.IO;
+
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        using var sr = new StreamReader(Console.OpenStandardInput());
+        using var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        int n = int.Parse(sr.ReadLine());
+        
+        sw.Write(n % 2 == 1 ? "SK" : "CY");
+    }
+}
