cacac42025589e73dc12d013c5c4a49f10b37199
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-12-18 15:51:31 +0900

    [Silver III] Title: N과 M (3), Time: 540 ms, Memory: 31068 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using System.Text;
+
+class Backjoon
+{
+    private static int N, M;
+    private static int[] result;
+    private static StringBuilder sb = new StringBuilder();
+    
+    static void Main(string[] args)
+    {
+        using var sr = new StreamReader(Console.OpenStandardInput());
+        using var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        string[] input = sr.ReadLine().Split();
+        N = int.Parse(input[0]);
+        M = int.Parse(input[1]);
+        
+        result = new int[M];
+        
+        DFS(0);
+
+        sw.Write(sb);
+    }
+
+    static void DFS(int depth)
+    {
+        if (depth == M)
+        {
+            for (int i = 0; i < M; i++)
+            {
+                sb.Append(result[i]).Append(" ");
+            }
+            sb.AppendLine();
+            return;
+        }
+
+        for (int i = 1; i <= N; i++)
+        {
+            result[depth] = i;
+            
+            DFS(depth + 1);
+        }
+    }
+}
