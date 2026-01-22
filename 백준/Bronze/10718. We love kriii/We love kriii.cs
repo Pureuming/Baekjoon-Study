c67db67c4294647d5f6f7e1790836b1fb64fce29
Pureuming <144452024+Pureuming@users.noreply.github.com>
2026-01-22 23:24:09 +0900

    [Bronze V] Title: We love kriii, Time: 48 ms, Memory: 5352 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,13 @@
+using System.IO;
+
+class Backjoon
+{
+    static void Main(string[] args)
+    {
+        using var sr = new StreamReader(Console.OpenStandardInput());
+        using var sw = new StreamWriter(Console.OpenStandardOutput());
+        
+        sw.WriteLine("강한친구 대한육군");
+        sw.WriteLine("강한친구 대한육군");
+    }
+}
